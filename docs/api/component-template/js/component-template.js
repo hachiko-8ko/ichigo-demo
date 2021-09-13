1917f2e08f46a66d989a8c71ef740d3425a2729a
hachiko-8ko <ynk.jeremy.lakatos@gmail.com>
2021-09-13 12:09:18 -0400

    Add component templating description
    The most foul-mouthed commit yet

---
+++ docs/api/component-template/js/component-template.js
@@ -0,0 +1,14 @@
+const BC = mi5.component.BoundComponent;
+const vm = {
+    planet: 'Betelgeuse',
+    advertising: `
+    <li>Luna</li>
+    <li>Mars</li>
+    <li>Jupiter System Union</li>
+    <li>Republique Venus</li>
+    `
+};
+const str = () => 'Earth';
+const c1 = new BC(vm, { selector: '#testComponent1' });
+const c2 = new BC(str, { selector: '#testComponent2', name: 'innerComponent' });
+const c3 = new BC(vm, { selector: '#testComponent3' });

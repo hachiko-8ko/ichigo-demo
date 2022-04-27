683574b0168031a98b6efd37ac37b6369c56862d
hachiko-8ko <ynk.jeremy.lakatos@gmail.com>
2022-04-27 09:51:48 -0400

    Add quick way (^) to access loopParent properties

--- docs/api/component-source/js/component-source.js
+++ docs/api/component-source/js/component-source.js
@@ -27,6 +27,10 @@ const vm2 = new ObservableState({ prop4: "Fourth" });
 const vm3 = "Fifth";
 const vm4 = "Sixth".toObservable();
 const vm5 = () => "Seventh";
+const vm6 = {
+    iterable: [2, 3, 5, 7],
+    description: 'is a prime'
+};
 
 const demoArea = document.getElementById('demoArea');
 BoundComponent.injectBind(vm1, "#demo1");
@@ -38,3 +42,5 @@ BoundComponent.injectBind(vm5, "#demo5");
 // To avoid manually calling render in the constructor, you can do this:
 // DescendantComponent.injectBind("Sir Not Used In This Example", "#demo6", { async: true });
 DescendantComponent.injectBind("Sir Not Used In This Example", "#demo6");
+
+BoundComponent.injectBind(vm6, "#demo7");
